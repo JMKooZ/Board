e00c33fd7fdb4dd9728ffec3df3824934a35cf4b
JMKooZ <ck03412@naver.com>
2023-07-07 16:19:53 +0900

    #7-paging Dto
    searchDto
    페이징의 기초가 되는 dto 이다.
    지금은 페이지 넘버에 대해서만 했지만 다음 시간에는 키워드를 통한 페이징 처리도 할거라 keyword 와 searchType이 담겨져있다.
    
    pagination
    현재페이지 마지막페이지 매퍼를 통해 가져온 값으로 페이지의 버튼 유무와 총 개수를 나누는 것을 계산하는 클래스이다.
    => 여기서 계산 수식을 잘 파악해야할것같다.
    => 우선순위를 잘못 생각해서 맨 마지막 엔드페이지 불리언 값에 대한 오류가 계속 터졌다..
    값이 왜 안나오는지 오랜시간 고민했고, 우선순위에 대해서 다시 알게되었다.
    
    pagingResponse
    postservice 와 postcontroller 에서 사용하기 위해 만들었다. 
    Map 을 통한 key-value 값도 사용할수있지만 list객체로 반환하여 처리를 진행해보았다.
    List<T> T는 어떤 타입의 객체든 상관없다는 의미

---
+++ src/main/java/com/study/board/domain/common/paging/PagingResponse.java
@@ -0,0 +1,16 @@
+package com.study.board.domain.common.paging;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class PagingResponse<T> {
+    private List<T> list = new ArrayList<>();
+    private Pagination pagination;
+
+    public PagingResponse(List<T> list, Pagination pagination) {
+        this.list.addAll(list);
+        this.pagination = pagination;
+    }
+}
