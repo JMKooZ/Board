062b0e938ab0488db8908aa376baf23765c3da16
JMKooZ <ck03412@naver.com>
2023-07-10 16:30:49 +0900

    #5-userService/userController
    댓글과 같은 방식으로 ajax 비동기 통신을 이용하여 처리하였다. 
    
    하지만 컨트롤러의 메소드의 전부가 ajax 방식으로 처리한게 아니라 @controller 어노테이션을 달아주었고 비동기 통신을 처리한 메소드는 @responsebody 어노테이션을 붙여주었다.

--- src/main/java/com/study/board/service/user/UserServiceImpl.java
+++ src/main/java/com/study/board/service/user/UserServiceImpl.java
@@ -1,39 +1,62 @@
 package com.study.board.service.user;
 
 import com.study.board.domain.user.UserMapper;
+import com.study.board.domain.user.UserRequest;
 import com.study.board.domain.user.UserResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
     private final UserMapper userMapper;
-
+    private final PasswordEncoder passwordEncoder;
     @Override
-    public void saveUser(UserResponse user) {
-        userMapper.saveUser(user);
+    public Long saveUser(UserRequest params) {
+        params.encodingPassword(passwordEncoder);
+        userMapper.saveUser(params);
+        return params.getUno();
     }
 
     @Override
-    public UserResponse findByUno(Long uno) {
-        return userMapper.findByUno(uno);
+    public UserResponse findById(String loginId) {
+        return userMapper.findById(loginId);
     }
 
     @Override
-    public void updateUser(UserResponse user) {
-
+    public Long update(UserRequest params) {
+        params.encodingPassword(passwordEncoder);
+        userMapper.update(params);
+        return params.getUno();
     }
 
     @Override
-    public void deleteUser(Long uno) {
-
+    public Long deleteByUno(Long uno) {
+        userMapper.deleteByUno(uno);
+        return uno;
     }
 
     @Override
-    public List<User> findAllUser() {
-        return null;
+    public int countById(String loginId) {
+        return userMapper.countById(loginId);
+    }
+
+    /**
+     * 로그인
+     * @param loginId - 로그인 ID
+     * @param password - 비밀번호
+     * @return 회원 상세정보
+     */
+    public UserResponse loginUser(final String loginId, final String password){
+        UserResponse user = userMapper.findById(loginId);
+        String encodedPassword = (user == null) ? "" : user.getPassword();
+
+        if(user == null || !passwordEncoder.matches(password, encodedPassword)){
+            return null;
+        }
+
+        user.clearPassword();
+        return user;
     }
 }
