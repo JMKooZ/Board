73960045ad30648827bd9a4ac6758586dc393f21
JMKooZ <ck03412@naver.com>
2023-07-11 13:06:08 +0900

    #15- fileDto / postRequest
    fileRequest
    
    파일의 정보를 저장할 때 필요한 컬럼은 file의 id 게시물의 id, 실제이름(눈에 보여주기 위해서), 파일이 저장되면서 생기는 저장 파일명 마지막으로 파일 크기
    
    생성자는 롬복의 @Builder 로 세 가지만 정해주었다. 
    롬복의 빌더는 순서를 달리해도 상관없다.
    
    setBno 메서드 만든 이유.
    게시물을 만들때 파일까지 같이 업로드 해주어야하는데 controller에서 set처리를 해주지 않으면 null 로 들어가 에러가 나게된다. 
    
    
    fileResponse 
    
    관리자가 파일의 정보가 다 필요할때 가져오는 response 를 정의 해주었다.
     
    postRequest
    파일을 업로드, 다운로드, 수정, 삭제 는 모두 게시물에서 이루어진다. 
    그래서 postRequest에 첨부파일의 리스트를 가지고 올수있게 list형태의 multipartfile 형으로 만들었고,
    삭제할때의 file id 값을 가지고 있는 list도 만들어 주었다.

---
+++ src/main/java/com/study/board/domain/file/FileResponse.java
@@ -0,0 +1,16 @@
+package com.study.board.domain.file;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class FileResponse {
+    private Long fno;                     // 파일 번호 (PK)
+    private Long bno;                     // 게시글 번호 (FK)
+    private String originalName;          // 원본 파일명
+    private String saveName;              // 저장 파일명
+    private long size;                    // 파일 크기
+    private Boolean deleteYn;             // 삭제 여부
+    private LocalDateTime createdDate;    // 생성일시
+    private LocalDateTime deletedDate;    // 삭제일시
+}
